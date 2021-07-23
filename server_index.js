42af9d4533b9474d508b5e067d44a4c20c418cde
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-07-22 22:53:05 -0500

    Main server file ready, to create the server in a local port (temporarily)

---
+++ server_index.js
@@ -0,0 +1,13 @@
+/**
+ * Main (begin) Node JS page of the Data API, which will start the server (local temporarily)
+ */
+const http = require('http');// Using Node JS 'http' module
+const requestHandler = require('./request-handler');// Require/import to use the handler file
+const server = http.createServer(requestHandler);// Creates the server giving the handler actions to the createServer method
+
+// Enables the server to listen requests in the local port 5000 (temporarily)
+server.listen(5000, () => {
+  console.log(
+    "Server is enabled on http://localhost:5000/"
+  );
+});
